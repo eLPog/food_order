394461907a068198af4c17ff9fb468b126b5f82d
LukasP <lukas.straubing@gmail.com>
2022-05-29 11:30:02 +0200

    header CartIcon component created

--- src/Components/UI/Header/Header.js
+++ src/Components/UI/Header/Header.js
@@ -1,12 +1,14 @@
 import styles from './Header.module.css';
 import headerImage from '../../../assets/headerPhoto.jpg';
+import { HeaderCartButton } from './HeaderCartButton';
 
 export function Header(props) {
   return (
     <>
       <header className={styles.header}>
+
         <h1>Food is good</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={styles['main-image']}>
         <img src={headerImage} alt="Vegetables on food" />
