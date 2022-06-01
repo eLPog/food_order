322c7f4e30779a4c3ce52eb9b36e5f174a95aa2c
LukasP <lukas.straubing@gmail.com>
2022-06-01 12:08:09 +0200

    Cart provider logic added, summary cart logic added, add and remove item functionality added

--- src/Components/Cart/Cart/CartModal.js
+++ src/Components/Cart/Cart/CartModal.js
@@ -3,13 +3,34 @@ import styles from './CartModal.module.css';
 import { Modal } from '../../UI/Modal/Modal';
 import { ShowCartContext } from '../../../Context/ShowCartContext/ShowCartContext';
 import { CartContext } from '../../../Context/CartContext/CartContext';
+import { CartItem } from '../CartItem/CartItem';
 
 export const CartModal = (props) => {
-  const { totalAmount, items } = useContext(CartContext);
+  const {
+    totalAmount, items, removeItem, addItem,
+  } = useContext(CartContext);
   const { showCartHandler } = useContext(ShowCartContext);
+
+  const cartItemRemoveHandler = (id) => {
+    removeItem(id);
+  };
+
+  const cartItemAddHandler = (item) => {
+    addItem({ ...item, amount: 1 });
+  };
+
   const cartItems = (
     <ul className={styles['cart-items']}>
-      {items.map((el)=><li className={styles['items-summary']} key={el.id}>{el.name}</li>)}
+      {items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
+      ))}
     </ul>
   );
   return (
@@ -17,11 +38,15 @@ export const CartModal = (props) => {
       {cartItems}
       <div className={styles.total}>
         <span>Total amount</span>
-        <span>{totalAmount.toFixed(2)}</span>
+        <span>
+          {totalAmount.toFixed(2)}
+          {' $'}
+        </span>
       </div>
       <div className={styles.actions}>
         <button className={styles['button--alt']} onClick={showCartHandler}>Close</button>
-        <button className={styles.button}>Order</button>
+        {items.length > 0 && <button className={styles.button}>Order</button>}
+
       </div>
     </Modal>
   );
