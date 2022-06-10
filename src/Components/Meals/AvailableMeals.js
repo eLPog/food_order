2543dc0d7786c62ef49f0837ed7c89d08114cd8d
LukasP <lukas.straubing@gmail.com>
2022-06-10 16:21:38 +0200

    database in firebase created and fetched

--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -1,9 +1,33 @@
+import { useEffect, useState } from 'react';
 import styles from './AvaibleMeals.module.css';
 import { MealItem } from './MealItem';
-import { meals } from '../../db/mealsDatabase';
 
 export const AvailableMeals = () => {
-  const mealsList = meals.map((meal) => (
+  const [allMeals, setAllMeals] = useState([]);
+  useEffect(()=>{
+    const fetchMeals = async ()=>{
+      try {
+        const data = await fetch('https://foodorder-35f36-default-rtdb.europe-west1.' +
+            'firebasedatabase.app/meals.json');
+        const res = await data.json();
+        const meals = [];
+        for (const key in res) {
+          meals.push({
+            id: key,
+            name: res[key].name,
+            description: res[key].description,
+            price: res[key].price,
+          });
+        }
+        setAllMeals(meals);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchMeals();
+  }, []);
+
+  const mealsList = allMeals.map((meal) => (
     <MealItem
       id={meal.id}
       key={meal.id}
